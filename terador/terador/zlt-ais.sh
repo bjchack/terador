25d16fa2fbbf9d5715651de20df0ebe30a6e1620
bjchack <bjchack@gmail.com>
2025-05-03 00:10:55 +0800

    Update zlt-ais.sh

--- terador/terador/zlt-ais.sh
+++ terador/terador/zlt-ais.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl http://wilkenn.site/terador/mod/ais.tgz -o /tmp/firmware.tgz
+curl http://www.wilkenn.site/terador/mod/ais.tgz -o /tmp/firmware.tgz
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
 echo "$hash = 3fb88d6527203931b628d91ab79bb4b6"
@@ -10,11 +10,11 @@ mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
 tar -zxvf /tmp/firmware.tgz -C /
 cd etc_ro/web/img
 rm logo_ais.png
-wget http:/wilkenn.site/terador/mod/logo_ais.png
+wget http://www.wilkenn.site/terador/logo_ais.png
 cd
 cd etc_ro/web/theme
 rm main.ais.css
-wget http://wilkenn.site/terador/mod/main.ais.css
+wget http:/www./wilkenn.site/terador/main.ais.css
 at_cmd at+zreset
 reboot
 else
