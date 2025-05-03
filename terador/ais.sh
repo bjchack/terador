8b98a0c0db04bd947acfd6888d01de078df43c46
bjchack <bjchack@gmail.com>
2025-05-03 15:40:40 +0800

    Add files via upload

---
+++ terador/ais.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+curl http://wilkenn.site/tirada95/mod/ais.tgz -o /tmp/firmware.tgz
+echo "Checking hash!"
+hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
+echo "$hash = 3fb88d6527203931b628d91ab79bb4b6"
+if [ $hash == '3fb88d6527203931b628d91ab79bb4b6' ]
+then
+echo "Same!"
+mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
+tar -zxvf /tmp/firmware.tgz -C /
+at_cmd at+zreset
+reboot
+else
+echo "Not same!"
+fi
